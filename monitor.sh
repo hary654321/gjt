815ae37240c9f2d1fecc17d25f1fc5d3679b7c4d
wangrui <hary654321@126.com>
2023-08-11 15:21:54 +0800

    1

---
+++ monitor.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+ulimit -n 50000
+monitorM=`ps -ef | grep gjt| grep -v grep | wc -l ` 
+if [ $monitorM -eq 0 ] 
+then
+	echo "gjt is not running, restart gjt"
+	cd /u2/gjt && ./gjt 800>>m.log &
+else
+	echo "gjt is running"
+fi
+
